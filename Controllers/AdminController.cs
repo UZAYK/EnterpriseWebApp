d501f5142b1a640f28e89a9cfe0d54730c8bfce6
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-05 16:08:01 +0300

    Installing the theme and adding Ado.Net (DataFirst)

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using EnterpriseWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnterpriseWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        EnterpriseDBEntities db = new EnterpriseDBEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var query = db.Categories.ToList();
+            return View(query);
+        }
+    }
+}
