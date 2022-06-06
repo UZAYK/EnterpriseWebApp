622307f04c215d4ba626123cfb87656e1a64f115
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-06 21:30:14 +0300

    About page

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,39 @@
+﻿using EnterpriseWebApp.Models.Context;
+using EnterpriseWebApp.Models.Model;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EnterpriseWebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        EnterpriseDB db = new EnterpriseDB();
+
+        public ActionResult Index()
+        {
+            var about = db.AboutUs.ToList();
+            return View(about);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var about = db.AboutUs.Where(x => x.AboutUsId == id).FirstOrDefault();
+            return View(about);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(AboutUs model, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var about = db.AboutUs.Where(x => x.AboutUsId == id).SingleOrDefault();
+                about.Description = model.Description;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
