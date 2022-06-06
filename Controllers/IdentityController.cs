b0a12c6efce46da54a0e13d4674f45e4da9c506a
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-06 20:32:54 +0300

    FILE UPLOAD TRANSACTIONS

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,8 +1,11 @@
 ﻿using EnterpriseWebApp.Models.Context;
+using EnterpriseWebApp.Models.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace EnterpriseWebApp.Controllers
@@ -20,28 +23,42 @@ namespace EnterpriseWebApp.Controllers
         // GET: Identity/Edit/5
         public ActionResult Edit(int id)
         {
-            var identity = db.Identities.Where(x => x.IdentityId ==
-            id).SingleOrDefault();
-
+            var identity = db.Identities.Where(x => x.IdentityId == id).SingleOrDefault();
             return View(identity);
         }
 
         // POST: Identity/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, IdentityController model, HttpPostedFileBase LogoURL)
+        public ActionResult Edit(int id, Identity model, HttpPostedFileBase LogoURL)
         {
-            try
+            if (ModelState.IsValid)
             {
-                //db.Identities.Add(IdentityController()
-                //{
+                var identity = db.Identities.Where(x => x.IdentityId == id).SingleOrDefault();
+                if (LogoURL != null)
+                {
+                    //file is valid?
+                    if (System.IO.File.Exists(Server.MapPath(identity.LogoURL)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(identity.LogoURL));
+                    }
+                    WebImage img = new WebImage(LogoURL.InputStream);
+                    FileInfo fileInfo = new FileInfo(LogoURL.FileName);
 
-                //});
+                    string logoName = Guid.NewGuid().ToString() + fileInfo.Extension;
+                    img.Resize(300, 200);
+                    img.Save("~/Uploads/Identity/" + logoName);
+
+                    identity.LogoURL = "/Uploads/Identity/" + logoName;
+                }
+                identity.Title = model.Title;
+                identity.Description = model.Description;
+                identity.Unvan = model.Unvan;
+                identity.Keywords = model.Keywords;
+                db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View(model);
         }
     }
 }
