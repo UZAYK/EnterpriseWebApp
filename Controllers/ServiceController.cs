767a260a0cac67bbd1a123cd7a18fd0d37ca35be
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-12 14:59:03 +0300

    Admin panel service table database operations...

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using EnterpriseWebApp.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnterpriseWebApp.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private EnterpriseDB db = new EnterpriseDB();
+
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View(db.Services.ToList());
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
