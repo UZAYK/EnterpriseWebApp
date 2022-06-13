99c7b1d72cf0bf1bffb5d7e9675f8833b97c417f
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-13 20:27:34 +0300

    Service transactions

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -1,8 +1,11 @@
 ﻿using EnterpriseWebApp.Models.Context;
+using EnterpriseWebApp.Models.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace EnterpriseWebApp.Controllers
@@ -21,5 +24,37 @@ namespace EnterpriseWebApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create(Service model, HttpPostedFileBase PictureURL)
+        {
+            if (ModelState.IsValid)
+            {
+                if (PictureURL != null)
+                {
+                    //file is valid?
+                    if (System.IO.File.Exists(Server.MapPath(model.PictureURL)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(model.PictureURL));
+                    }
+                    WebImage img = new WebImage(PictureURL.InputStream);
+                    FileInfo fileInfo = new FileInfo(PictureURL.FileName);
+
+                    string logoName = Guid.NewGuid().ToString() + fileInfo.Extension;
+                    img.Resize(300, 200);
+                    img.Save("~/Uploads/Service/" + logoName);
+
+                    model.PictureURL = "/Uploads/Service/" + logoName;
+                }
+                db.Services.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
     }
 }
